eb4a19d48416e435233bdd3ebbffa24d6340eea7
BT_2N2_05 <example@bths.edu>
2023-12-15 13:43:40 -0500

    commit 2 partner 2

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("stevenzthebest");
         System.out.println("ivanlthebestest");
+        System.out.println("LLLLLLL");
     }
 }
